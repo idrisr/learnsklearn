2ad09a3d2d1a1e569fb2cbe9a835fcd130a10492
Idris Raja <idris.raja@gmail.com>
2021-08-17 11:35:09 -0500

    test on width

--- test/05_count_vectorizer_test.py
+++ test/05_count_vectorizer_test.py
@@ -6,10 +6,12 @@ import numpy as np
 
 # Convert a collection of text documents to a matrix of token counts
 
+sentence = 'it was the best of times it was the blurst of times'
 
 def corpus_strategy():
     return lists(min_size=2, 
-            elements=lists(elements=sampled_from('welcome to the jungle'.split()),
+            elements=lists(elements=sampled_from('it was the best of times, it\
+                was the blurst of times'.split()),
                 min_size=4).map(lambda x: " ".join(x))
             )
 
@@ -18,3 +20,4 @@ def test_vectorizer(c):
     v = CountVectorizer(analyzer='word')
     x = v.fit_transform(c)
     assert x.toarray().shape[0] == len(c)
+    assert x.toarray().shape[1] <= len(set(sentence.split()))
