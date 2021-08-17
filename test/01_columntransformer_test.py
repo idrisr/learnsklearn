bdc1c2065d92f9ce67d61e3cd089da689f2170e7
Idris Raja <idris.raja@gmail.com>
2021-08-17 11:28:17 -0500

    count vectorizer

--- test/01_columntransformer_test.py
+++ test/01_columntransformer_test.py
@@ -1,27 +1,40 @@
 from hypothesis.strategies import *
+from hypothesis import given
 from sklearn.compose import ColumnTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import OrdinalEncoder
+from hypothesis.extra.pandas import column, data_frames, range_indexes
 import pandas as pd
+import numpy as np
+
 
 #  applies transformers to columns of an array or pandas DataFrame
-
-df = pd.DataFrame(
-        {'city': ['London', 'London', 'Paris', 'London'],
-            'title': ["His Last Bow", "How Watson Learned the Trick",
-                "A Moveable Feast", "The Grapes of Wrath"],
-            'expert_rating': [5, 3, 4, 5],
-            'user_rating': [4, 5, 4, 3]})
+def make_df():
+    return data_frames([
+        column('city', elements=sampled_from(['chicago', 'london', 'new york'])),
+        column('B', dtype=float)], index=range_indexes(min_size=40))
 
 
-ct = ColumnTransformer(
-    [
-        ('city_category', OneHotEncoder(dtype='int'), ['city'])
-        ],
-    remainder='passthrough')
+@given(make_df())
+def test_column_transform(df):
+    n_uniq = len(df.city.unique())
+    ct = ColumnTransformer(
+        [
+            ('city_category', OneHotEncoder(dtype=np.int32), ['city'])
+            ],
+        remainder='passthrough')
+    t = ct.fit_transform(df)
+    assert np.sum(t[:, :n_uniq]) == df.shape[0]
 
 
-#  ct.fit(df)
-#  ct.get_feature_names()
-#  ct.transform(df).toarray()
+@given(make_df())
+def test_column_transform2(df):
+    n_uniq = len(df.city.unique())
+    ct = ColumnTransformer(
+        [
+            ('city_category', OrdinalEncoder(dtype=np.int32), ['city'])
+            ],
+        remainder='passthrough')
+    t = ct.fit_transform(df)
+    assert np.max(t[:, 0]) == n_uniq -1
